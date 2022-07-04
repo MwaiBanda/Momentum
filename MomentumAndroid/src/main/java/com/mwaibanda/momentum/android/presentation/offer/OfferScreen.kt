ca872ef2cd31b2db5576c945250c28eb2dea2c72
Mwai Banda <bandamwai@gmail.com>
2022-07-03 21:36:09 -0500

    Android: Added KeyPad

--- MomentumAndroid/src/main/java/com/mwaibanda/momentum/android/presentation/offer/OfferScreen.kt
+++ MomentumAndroid/src/main/java/com/mwaibanda/momentum/android/presentation/offer/OfferScreen.kt
@@ -4,6 +4,10 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,21 +23,72 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mwaibanda.momentum.android.presentation.components.BlurredBackground
 
 @Composable
 fun OfferScreen(){
-    BlurredBackground {
+//    BlurredBackground {
+    val buttons = arrayOf(
+        arrayOf("1", "2", "3"),
+        arrayOf("4", "5", "6"),
+        arrayOf("7", "8", "9"),
+        arrayOf("•", "0", "<"),
+        )
         Column(
-            Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
+            Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
+            Spacer(modifier = Modifier.height(10.dp))
             Text(
-                text = "OfferScreen",
-                color = Color.White,
+                text = "$0",
+                fontSize = 100.sp
             )
+            Column(
+                Modifier
+                    .background(Color.White),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                buttons.forEach { row ->
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Row {
+                        row.forEach { button ->
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Box(contentAlignment = Alignment.Center) {
+                                Box(
+                                    modifier = Modifier
+                                        .size(80.dp)
+                                        .clip(CircleShape)
+                                )
+                                Text(
+                                    text = button,
+                                    fontSize = 40.sp
+                                )
+                            }
+                            Spacer(modifier = Modifier.width(10.dp))
+
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth(0.85f)
+                        .height(55.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFE55F1F))
+                ) {
+                    Text(text = "Offer")
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+            }
         }
-    }
+
 }
 
 @Composable
